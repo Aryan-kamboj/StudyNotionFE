1fd1d1855b16345cbf9b5e0b02eae359aed0e8a8
Aryan_kamboj <aryankambozz@gmail.com>
2023-11-08 15:12:10 +0530

    made cart responsive

--- src/components/dashboardPages/StudentTabs/Cart.jsx
+++ src/components/dashboardPages/StudentTabs/Cart.jsx
@@ -19,7 +19,7 @@ export const Cart = () => {
     // console.log(cart);
   return (
     <div className='h-fit '>
-          <div className='ml-8 mt-8 '>
+          <div className='ml-8 max-tablet:mx-2 mt-8 '>
             <div className='text-sm space-y-4 '>
               <span className='text-richblack-300'>Home / Dashboard /</span>
               <span className='text-yellow-50'> Cart</span>
@@ -28,7 +28,7 @@ export const Cart = () => {
             <div className=' text-richblack-300 pb-3 border-b-[1px] border-richblack-700'>
               {cart.length} cources in cart.
             </div>
-            <div style={{height:`75vh`}} className=' flex  justify-between relative '>
+            <div style={{height:`75vh`}} className='flex justify-between relative max-tablet:flex-col-reverse'>
               <div className='basis-[70%] hideScrollBars scroll-smooth overflow-scroll'>
                 {cart.map((course,i)=>{
                     const {
@@ -39,23 +39,23 @@ export const Cart = () => {
                     reviewCount,
                     thumbnail,
                     instructorName}=course;
-                    return (<div key={i} className='cursor-pointer h-[14rem] items-center relative -top-[1px] justify-between flex border-t-[1px] border-richblack-700'>
-                      <div className='flex space-x-6'>
-                        <img className='w-[15rem] h-[10rem] object-cover rounded-xl' src={thumbnail} alt={title} />
+                    return (<div key={i} className='cursor-pointer h-[14rem] max-tablet:h-[16rem] py-4 items-center relative -top-[1px] justify-between flex max-tablet:flex-col border-t-[1px] border-richblack-700'>
+                      <div className='flex space-x-6 '>
+                        <img className='w-[15rem] max-tablet:w-[5rem] h-[10rem] object-cover rounded-xl' src={thumbnail} alt={title} />
                         <div className='space-y-4'>
                           <h1 className='text-2xl font-[500]'>{title}</h1>
                           <p>{instructorName}</p>
                           <div className='flex items-center space-x-3'><span className='text-yellow-100'>{rating}</span><RatingStars rating={rating}/><span className='text-richblack-400'>{reviewCount} reviews</span></div>
                         </div>
                       </div>
-                      <div className='flex flex-col h-[10rem] justify-between items-center'>
+                      <div className='flex flex-col max-tablet:mt-4 max-tablet:flex-row max-tablet:w-[90%] h-[10rem] max-tablet:h-[4rem] justify-between items-center'>
                         <button onClick={(e)=>{dispatcher(removeFromCart(e.currentTarget.attributes.courseid.value))}} courseid={id} className='space-x-2 flex bg-richblack-800 text-pink-200 items-center p-3 border-richblack-700 border-[1px] rounded-xl'><FiTrash2/><span>Remove</span></button>
                         <p className='text-2xl font-[500] text-yellow-100'>Rs.{price}</p>
                       </div>
                     </div>)
                   })}
               </div>
-              <div className='w-[14rem] h-[10rem] basis-[20%] rounded-lg flex flex-col justify-evenly bg-richblack-700 p-4 absolute top-6 right-[3.5rem]'>
+              <div className='w-[14rem] h-[10rem] max-tablet:w-[100%] max-tablet:relative max-tablet:right-0 basis-[20%] rounded-lg flex flex-col justify-evenly bg-richblack-700 p-4 absolute top-6 right-[3.5rem]'>
                   <h1 className='text-richblack-200'>Total:</h1>
                   <p className='text-3xl text-yellow-50'>Rs. {total}</p>
                   <StdButton handler={buyNowHandler} width={100} color="yellow" >Buy Now</StdButton>
