05a359ecc22e90f0e0e3745ead6bf7ee1ae0f0f6
Aryan_kamboj <aryankambozz@gmail.com>
2023-11-08 15:14:39 +0530

    made cart responsive

--- src/components/dashboardPages/StudentTabs/EnrolledCourses.jsx
+++ src/components/dashboardPages/StudentTabs/EnrolledCourses.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { EnrolledCoursesData } from '../../../data/tempData'
 import { Link } from 'react-router-dom'
 import {BsThreeDotsVertical} from "react-icons/bs"
